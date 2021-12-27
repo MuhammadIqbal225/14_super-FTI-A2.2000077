7b48f7097acba70b28884f63265715daf20d5550
Muhammad Iqbal <a2.2000077@mhs.stmik-sumedang.ac.id>
2021-12-27 15:03:37 +0700

    firs commit

---
+++ main.py
@@ -0,0 +1,25 @@
+class Hero:
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
+
+    def showInfo(self):
+        print ("{} dengan health: {}".format(self.name,self.health))
+
+class Hero_intelligent(Hero):
+    def __init__(self, name):
+        #Hero.__init__(self,  name, 100)
+        super().__init__(name, 100)
+        super().showInfo()
+
+class Hero_strength(Hero):
+    def __init__(self, name):
+       super().__init__(name, 200)
+       super().showInfo()
+
+yesi = Hero_intelligent('yesi')
+axe = Hero_strength('axe')
+
+print(yesi.name , " " , yesi.health)
+print(axe.name , " " , axe.health)
+        
